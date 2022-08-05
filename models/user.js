fcfe113d492116b510b3913b842f2d4b89c3e8ed
brentaharris <brentaustinharris@gmail.com>
2022-08-05 11:36:11 -0400

    passport jwt

--- models/user.js
+++ models/user.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose')
+
+//handles user, password schemas, also hashes & salts passwords
+const passportLocalMongoose = require('passport-local-mongoose')
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+    
     admin: {
         type: Boolean,
         default: false
     }
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', userSchema)
