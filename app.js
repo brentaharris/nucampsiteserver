803fe8929285f83452207bbe5a6893762bee714f
brentaharris <brentaustinharris@gmail.com>
2022-08-06 16:42:35 -0400

    file upload endpoint added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter')
 
 const url = config.mongoUrl
 const connect = mongoose.connect(url, {
@@ -53,11 +54,12 @@ app.use('/users', usersRouter);
 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+app.use('/campsites', campsiteRouter)
+app.use('/promotions', promotionRouter)
+app.use('/partners', partnerRouter)
+app.use('/imageUpload', uploadRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,7 +70,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
   res.status(err.status || 500);
